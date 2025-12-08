de2013d17fe201f4ac311ff840a2a647867540b6
Win.10-2 <61893884+developermodoki@users.noreply.github.com>
2025-12-08 16:44:15 +0900

    Added DHT11

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,9 +1,31 @@
+#include <DHT.h>
+
+#define DHT_INPUT_PIN 33
+#define DHT_TYPE DHT11
+
+DHT dht(DHT_INPUT_PIN, DHT_TYPE);
+
 void setup() {
   Serial.begin(9600);
+  //pinMode(21, INPUT);
+  dht.begin();
 
 }
 
 void loop() {
-  Serial.println("hello world");
   delay(2000);
+  Serial.println("Reading Temperature.");
+
+  float humid = dht.readHumidity();
+  float temp = dht.readTemperature();
+
+  
+  if (isnan(humid) || isnan(temp)) {
+    Serial.println("Failed to reading");
+    return;
+  }
+
+  Serial.printf("Temp: %.2f\n", temp);
+  Serial.printf("Humid: %.2f\n", humid);
+  
 }
