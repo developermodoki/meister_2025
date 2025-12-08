ace49b3301f23120b181ac623a7de779bead563d
Win.10-2 <61893884+developermodoki@users.noreply.github.com>
2025-12-09 04:00:37 +0900

    Added Network Connection

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,15 +1,50 @@
+#pragma once
 #include <DHT.h>
+#include <WiFi.h>
+#include <time.h>
 
+
+//#define SSID "KOKI08-DYNABOOK 1420"
+//#define WIFI_PASS "653R0o0<"
+//dummy ssid&pass
 #define DHT_INPUT_PIN 33
 #define DHT_TYPE DHT11
+#define NTP_1 "ntp.nict.jp"
+#define NTP_2 "ntp1.noc.titech.ac.jp"
+#define NTP_3 "ntp.jst.mfeed.ad.jp"
+
 
 DHT dht(DHT_INPUT_PIN, DHT_TYPE);
 
 void setup() {
   Serial.begin(9600);
-  //pinMode(21, INPUT);
   dht.begin();
 
+  // https://github.com/developermodoki/meister_2024/blob/main/atoms3_main/atoms3_main.ino#L205C5-L205C8
+  /* BEGIN WIFI-CONNECTION */
+  Serial.println("Starting Wi-Fi Connection...");
+  WiFi.begin(SSID, WIFI_PASS);
+  delay(2000);
+
+  if(!WiFi.isConnected()) {
+    int i = 0;
+    while(WiFi.begin(SSID, WIFI_PASS) != WL_CONNECTED) {
+      delay(2000);
+      i++;
+      if(i <= 3) break;
+    };
+  }
+
+  if(WiFi.isConnected()) {
+    Serial.print("Wi-Fi Connected. Local IP is: ");
+    Serial.println(WiFi.localIP());
+    configTime(60 * 60 * 9, 0, NTP_1, NTP_2, NTP_3);
+  }
+  else {
+    Serial.println("Wifi Connection failed.");
+  }
+
+  /* END WIFI-CONNECTION */
 }
 
 void loop() {
@@ -21,7 +56,7 @@ void loop() {
 
   
   if (isnan(humid) || isnan(temp)) {
-    Serial.println("Failed to reading");
+    Serial.println("Failed reading");
     return;
   }
 
