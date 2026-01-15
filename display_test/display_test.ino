4a496b26532ed07958cf4200f151d3368a1ac5ba
Win.10-2 <61893884+developermodoki@users.noreply.github.com>
2026-01-15 11:06:10 +0900

    Updated display options

--- display_test/display_test.ino
+++ display_test/display_test.ino
@@ -23,13 +23,22 @@ void loop() {
   }
 } 
 
+char flag = 2;
 void Main() {
-  CoreS3.Display.fillScreen(TFT_BLACK);
-  CoreS3.Display.setCursor(10, 50);
-  CoreS3.Display.println("ステータス: 正常");
-  CoreS3.Display.setCursor(10, 80);
-  CoreS3.Display.printf("気温差: %0.2f °C", 0.223);
-  CoreS3.Display.setCursor(10, 120);
-  CoreS3.Display.printf("湯の温度: %0.1f °C", 40.00);
+  if(flag == 1) {
+    CoreS3.Display.fillScreen(TFT_BLACK);
+    CoreS3.Display.setCursor(10, 50);
+    CoreS3.Display.println("ステータス: 正常");
+    CoreS3.Display.setCursor(10, 80);
+    CoreS3.Display.printf("気温差: %0.2f °C", 0.223);
+    CoreS3.Display.setCursor(10, 120);
+    CoreS3.Display.printf("湯の温度: %0.1f °C", 40.00);
+  }
+  else if(flag == 2) {
+    CoreS3.Display.fillScreen(TFT_WHITE);
+    CoreS3.Display.setTextColor(TFT_RED);
+    CoreS3.Display.setCursor(10, 50);
+    CoreS3.Display.println("警告: example");
+  }
 
 }
