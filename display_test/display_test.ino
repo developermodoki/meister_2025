298ed709b487385fd42e716c71dc2efad05de77a
Win.10-2 <61893884+developermodoki@users.noreply.github.com>
2026-01-18 06:01:50 +0900

    Update display_test.ino

--- display_test/display_test.ino
+++ display_test/display_test.ino
@@ -11,7 +11,6 @@ void setup() {
   CoreS3.Display.setTextFont(&fonts::lgfxJapanGothic_32);
 }
 
-void Main();
 
 void loop() {
   unsigned long currentTime = millis();
@@ -24,7 +23,7 @@ void loop() {
   
 } 
 
-char flag = 1;
+char flag = 3;
 void Main() {
   if(flag == 1) {
     CoreS3.Display.fillScreen(TFT_BLACK);
@@ -33,13 +32,24 @@ void Main() {
     CoreS3.Display.setCursor(10, 80);
     CoreS3.Display.printf("気温差: %0.2f °C", 0.223);
     CoreS3.Display.setCursor(10, 120);
-    CoreS3.Display.printf("湯の温度: %0.1f °C", 40.00);
+    CoreS3.Display.printf("湯の温度: %0.1f °C", 40.00); // dummy temperature
   }
   else if(flag == 2) {
     CoreS3.Display.fillScreen(TFT_WHITE);
     CoreS3.Display.setTextColor(TFT_RED);
     CoreS3.Display.setCursor(10, 50);
-    CoreS3.Display.println("警告: example");
+    CoreS3.Display.print("警告: お湯の温度が高すぎます");
+    CoreS3.Display.setCursor(10, 120);
+    CoreS3.Display.print("41°C以下に下げてください"); 
   }
+  else if(flag == 3) {
+    CoreS3.Display.fillScreen(TFT_WHITE);
+    CoreS3.Display.setTextColor(TFT_RED);
+    CoreS3.Display.setCursor(10, 50);
+    CoreS3.Display.print("警告: 部屋間の気温差が10°C以上です");
+    CoreS3.Display.setCursor(10, 120);
+    CoreS3.Display.printf("気温差: %0.1f °C", 10.1);
+  }
+  
 
 }
